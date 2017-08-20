f5db09fdc48180970e774838438cd1fdc142e44a
Courtney Treweek <c.k.treweek@gmail.com>
2017-08-20 16:19:56 -0400

    Add search by city filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @cities = get_cities
+    @artists = Artist.all
   end
+
+  def get_cities
+    @cities = ["Choose a city"]
+    @artists = Artist.all
+    @artists.each do |artist|
+    @cities << artist.city
+    end
+    @cities.uniq
+  end
+
 end
