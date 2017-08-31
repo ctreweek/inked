e948b025151c03960ea3dc7f4072c328121ff303
Courtney Treweek <c.k.treweek@gmail.com>
2017-08-31 18:01:17 -0400

    Create message and conversation views, add logic to display sender's name and display message

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,5 +5,8 @@ class Conversation < ApplicationRecord
   validates_uniqueness_of :user_id, :scope => :artist_id
   validates_uniqueness_of :artist_id, :scope => :user_id
 
-
+  scope :between, ->(artist_id, user_id) {
+    where ('(conversations.artist_id = artist_id AND conversations.user_id = user_id) OR
+    (conversations.user_id = user_id AND conversations.artist_id = artist_id)')
+  }
 end
